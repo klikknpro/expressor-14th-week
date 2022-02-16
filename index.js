31ffb47e248b5d7d27cb7c308eb7c061f0a80b38
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-16 11:18:38 +0100

    basic authentication

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 
 const users = require("./users.json");
 
+// dummy sign up
 app.post("/api/signup", (req, res) => {
   if (!req.body.username || !req.body.password) return res.status(400).json("Missing input");
   const existingUser = users.some((user) => user.username === req.body.username);
@@ -16,10 +17,42 @@ app.post("/api/signup", (req, res) => {
   const newUser = {
     username: req.body.username,
     password: req.body.password,
+    todos: [],
   };
   users.push(newUser);
   fs.writeFileSync("users.json", JSON.stringify(users));
   res.status(200).json("Signed up successfully");
+});
+
+// authorize and respond data
+app.get("/api/todo", (req, res) => {
+  const authorization = req.header("Authorization");
+  if (!authorization) return res.sendStatus(401);
+
+  const username = authorization.split(":::")[0];
+  const password = authorization.split(":::")[1];
+
+  const validUser = users.find((user) => user.username === username && user.password === password);
+  if (!validUser) return res.sendStatus(401);
+  res.json({ message: validUser.todos });
+});
+
+// authorize and append data
+app.post("/api/todo", (req, res) => {
+  const authorization = req.header("Authorization");
+  if (!authorization) return res.sendStatus(401);
+
+  const username = authorization.split(":::")[0];
+  const password = authorization.split(":::")[1];
+
+  const validUser = users.find((user) => user.username === username && user.password === password);
+  if (!validUser) return res.sendStatus(401);
+
+  const todo = req.body.todo;
+  if (!todo) return res.sendStatus(400);
+  validUser.todos.push(todo);
+  fs.writeFileSync("users.json", JSON.stringify(users));
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
