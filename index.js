8a669e0e38ef3881e796d14d76180892bca854a0
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-21 11:20:19 +0100

    authentication is not good yet

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.json());
 
 const users = require("./users.json");
 
+// ez nem a front-end sessionStorage cucc!!!
+let mySessionStorage = {};
+
 // dummy sign up
 app.post("/api/signup", (req, res) => {
   if (!req.body.username || !req.body.password) return res.status(400).json("Missing input");
@@ -39,13 +42,10 @@ app.get("/api/todo", (req, res) => {
 
 // authorize and append data
 app.post("/api/todo", (req, res) => {
-  const authorization = req.header("Authorization");
-  if (!authorization) return res.sendStatus(401);
+  const sessionId = req.header("Authorization");
+  if (!sessionId) return res.sendStatus(401);
 
-  const username = authorization.split(":::")[0];
-  const password = authorization.split(":::")[1];
-
-  const validUser = users.find((user) => user.username === username && user.password === password);
+  const validUser = mySessionStorage[sessionId];
   if (!validUser) return res.sendStatus(401);
 
   const todo = req.body.todo;
@@ -55,7 +55,7 @@ app.post("/api/todo", (req, res) => {
   res.sendStatus(200);
 });
 
-// valami bejelentkezo valami
+// bejelentkezes
 app.post("/api/login", (req, res) => {
   const authorization = req.header("Authorization");
   if (!authorization) return res.sendStatus(401);
@@ -66,7 +66,18 @@ app.post("/api/login", (req, res) => {
   const validUser = users.find((user) => user.username === username && user.password === password);
   if (!validUser) return res.sendStatus(401);
 
-  res.sendStatus(200);
+  const sessionId = Math.random().toString();
+  mySessionStorage[sessionId] = validUser;
+  console.log(mySessionStorage);
+
+  // lenullazni 30 sec utan
+  setTimeout(() => {
+    console.log("the end");
+    delete mySessionStorage[sessionId];
+  }, 10 * 1000);
+
+  // most mar csak a random sessionId megy vissza a FE-re
+  res.json(sessionId);
 });
 
 app.listen(port, () => {
